79efd65177882a5abc483d591339eb1fd173f8ce
mateus <mateus_sat@hotmail.com>
2019-06-02 20:52:50 -0300

    Criação de Navigation

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
-import MainTabNavigator from './MainTabNavigator';
 
-export default createAppContainer(createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
-}));
+import LoginScreen from '../screens/LoginScreen'
+import RegisterScreen from '../screens/RegisterScreen';
+
+export default createAppContainer(createSwitchNavigator(
+  {
+    Login: LoginScreen,
+    Register: RegisterScreen,
+  },
+  {
+    initialRouteName: 'Login',
+  }
+));
