25aee533033bcad8717a7ec3144c0ab7987b81e7
mateus <mateus_sat@hotmail.com>
2019-06-03 13:04:15 -0300

    importação do axios e criação do post

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import {
-  Text,
-  TextInput,
-  View,
-  StyleSheet,
-  ImageBackground,
-  Image,
-  Dimensions,
-  TouchableOpacity,
-  Button,
+    Text,
+    TextInput,
+    View,
+    StyleSheet,
+    ImageBackground,
+    Image,
+    Dimensions,
+    TouchableOpacity,
+    Button,
 } from 'react-native';
 
 import bgimage from '../assets/images/Android-BackgroundChart.png';
@@ -17,165 +17,164 @@ import Icon from 'react-native-vector-icons/Ionicons';
 const { width: WIDTH } = Dimensions.get('window');
 
 export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      showPass: true,
-      press: false,
-    };
-  }
-
-  showPass = () => {
-    if (this.state.press == false) {
-      this.setState({ showPass: false, press: true });
-    } else {
-      this.setState({ showPass: true, press: false });
+    constructor() {
+        super();
+        this.state = {
+            showPass: true,
+            press: false,
+        };
     }
-  };
 
-  render() {
-    return (
-      <ImageBackground source={bgimage} style={styles.backgroundContainer}>
-        <View styles={styles.logoContainer}>
-          <Image source={logoimage} style={styles.logo} />
-          <Text style={styles.logoText}>CARWASH</Text>
-        </View>
+    showPass = () => {
+        if (this.state.press == false) {
+            this.setState({ showPass: false, press: true });
+        } else {
+            this.setState({ showPass: true, press: false });
+        }
+    };
 
-        <View style={styles.inputContainer}>
-          <Icon
-            name={'ios-person'}
-            size={28}
-            color={'rgba(255,255,255, 0.7)'}
-            style={styles.inputicone}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder={'Username'}
-            placeholderTextColor={'rgba(255,255,255,0.7)'}
-            underlineColorAndroid="transparent"
-          />
-        </View>
+    render() {
+        return (
+            <ImageBackground source={bgimage} style={styles.backgroundContainer}>
+                <View styles={styles.logoContainer}>
+                    <Image source={logoimage} style={styles.logo} />
+                    <Text style={styles.logoText}>CARWASH</Text>
+                </View>
 
-        <View style={styles.inputContainer}>
-          <Icon
-            name={'ios-lock'}
-            size={28}
-            color={'rgba(255,255,255, 0.7)'}
-            style={styles.inputicone}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder={'Senha'}
-            secureTextEntry={this.state.showPass}
-            placeholderTextColor={'rgba(255,255,255,0.7)'}
-            underlineColorAndroid="transparent"
-          />
+                <View style={styles.inputContainer}>
+                    <Icon
+                        name={'ios-person'}
+                        size={28}
+                        color={'rgba(255,255,255, 0.7)'}
+                        style={styles.inputicone}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder={'Username'}
+                        placeholderTextColor={'rgba(255,255,255,0.7)'}
+                        underlineColorAndroid="transparent"
+                    />
+                </View>
 
-          <TouchableOpacity
-            style={styles.btnEys}
-            onPress={this.showPass.bind(this)}>
-            <Icon
-              name={this.state.showPass == false ? 'ios-eye' : 'ios-eye-off'}
-              size={26}
-              color={'rgba(255,255,255,0.7)'}
-            />
-          </TouchableOpacity>
-        </View>
+                <View style={styles.inputContainer}>
+                    <Icon
+                        name={'ios-lock'}
+                        size={28}
+                        color={'rgba(255,255,255, 0.7)'}
+                        style={styles.inputicone}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder={'Senha'}
+                        secureTextEntry={this.state.showPass}
+                        placeholderTextColor={'rgba(255,255,255,0.7)'}
+                        underlineColorAndroid="transparent"
+                    />
 
-        <TouchableOpacity style={styles.btnLogin}>
-          <Text style={styles.text}>Logar</Text>
-        </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.btnEys}
+                        onPress={this.showPass.bind(this)}>
+                        <Icon
+                            name={this.state.showPass == false ? 'ios-eye' : 'ios-eye-off'}
+                            size={26}
+                            color={'rgba(255,255,255,0.7)'}
+                        />
+                    </TouchableOpacity>
+                </View>
 
-        <TouchableOpacity
-          style={styles.btnCadastrar}
-          onPress={() => {
-            this.props.navigation.navigate('Register');
-          }}>
-          <Text style={styles.text}>Cadastrar</Text>
-        </TouchableOpacity>
-      </ImageBackground>
-    );
-  }
+                <TouchableOpacity style={styles.btnLogin}>
+                    <Text style={styles.text}>Logar</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.btnCadastrar}
+                    onPress={() => {
+                        this.props.navigation.navigate('Register');
+                    }}>
+                    <Text style={styles.text}>Cadastrar</Text>
+                </TouchableOpacity>
+            </ImageBackground>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  backgroundContainer: {
-    flex: 1,
-    width: null,
-    height: null,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    backgroundContainer: {
+        flex: 1,
+        width: null,
+        height: null,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 
-  logoContainer: {
-    alignItems: 'center',
-    marginBottom: 50,
-  },
+    logoContainer: {
+        alignItems: 'center',
+        marginBottom: 50,
+    },
 
-  logo: {
-    width: 170,
-    height: 120,
-  },
+    logo: {
+        width: 170,
+        height: 120,
+    },
 
-  logoText: {
-    alignItems: 'center',
-    color: 'white',
-    fontSize: 35,
-    fontWeight: '500',
-    marginTop: 0.1,
-    marginBottom: 50,
-    opacity: 0.7,
-  },
+    logoText: {
+        alignItems: 'center',
+        color: 'white',
+        fontSize: 35,
+        fontWeight: '500',
+        marginTop: 0.1,
+        marginBottom: 50,
+        opacity: 0.7,
+    },
 
-  inputContainer: {
-    marginTop: 10,
-  },
+    inputContainer: {
+        marginTop: 10,
+    },
 
-  input: {
-    width: WIDTH - 55,
-    height: 45,
-    borderRadius: 25,
-    fontSize: 16,
-    paddingLeft: 45,
-    //marginTop: 80,
-    backgroundColor: 'rgba(0,0,0,0.35)',
-    color: 'rgba(255,255,255,0.7)',
-    marginHorizontal: 25,
-  },
+    input: {
+        width: WIDTH - 55,
+        height: 45,
+        borderRadius: 25,
+        fontSize: 16,
+        paddingLeft: 45,
+        backgroundColor: 'rgba(0,0,0,0.35)',
+        color: 'rgba(255,255,255,0.7)',
+        marginHorizontal: 25,
+    },
 
-  inputicone: {
-    position: 'absolute',
-    top: 8,
-    left: 37,
-  },
+    inputicone: {
+        position: 'absolute',
+        top: 8,
+        left: 37,
+    },
 
-  btnEys: {
-    position: 'absolute',
-    top: 8,
-    right: 37,
-  },
+    btnEys: {
+        position: 'absolute',
+        top: 8,
+        right: 37,
+    },
 
-  btnLogin: {
-    width: WIDTH - 55,
-    height: 45,
-    borderRadius: 25,
-    backgroundColor: '#432577',
-    justifyContent: 'center',
-    marginTop: 20,
-  },
+    btnLogin: {
+        width: WIDTH - 55,
+        height: 45,
+        borderRadius: 25,
+        backgroundColor: '#432577',
+        justifyContent: 'center',
+        marginTop: 20,
+    },
 
-  btnCadastrar: {
-    width: WIDTH - 55,
-    height: 45,
-    borderRadius: 25,
-    backgroundColor: '#432577',
-    justifyContent: 'center',
-    marginTop: 20,
-  },
+    btnCadastrar: {
+        width: WIDTH - 55,
+        height: 45,
+        borderRadius: 25,
+        backgroundColor: '#432577',
+        justifyContent: 'center',
+        marginTop: 20,
+    },
 
-  text: {
-    color: 'rgba(255,255,255,0.7)',
-    fontSize: 16,
-    textAlign: 'center',
-  },
+    text: {
+        color: 'rgba(255,255,255,0.7)',
+        fontSize: 16,
+        textAlign: 'center',
+    },
 });
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,162 +1,199 @@
 import React, { Component } from 'react';
 import {
-  Text,
-  TextInput,
-  View,
-  StyleSheet,
-  ImageBackground,
-  Image,
-  Dimensions,
-  TouchableOpacity,
+    Text,
+    TextInput,
+    View,
+    StyleSheet,
+    ImageBackground,
+    Image,
+    Dimensions,
+    TouchableOpacity,
 } from 'react-native';
 
 import bgimage from '../assets/images/Android-BackgroundChart.png';
 import logoimage from '../assets/images/e4ec2acf005fe764835f1ffb1a00e4b4-bolhas-cobertas-icone-de-carro.png';
 import Icon from 'react-native-vector-icons/Ionicons';
+import axios from 'axios';
 const { width: WIDTH } = Dimensions.get('window');
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <ImageBackground source={bgimage} style={styles.backgroundContainer}>
-        <View styles={styles.logoContainer}>
-          <Image source={logoimage} style={styles.logo} />
-          <Text style={styles.logoText}>CADASTRO</Text>
-        </View>
 
-        <View style={styles.inputContainer}>
-          <Icon
-            name={'ios-person'}
-            size={28}
-            color={'rgba(255,255,255, 0.7)'}
-            style={styles.inputicone}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder={'Username'}
-            placeholderTextColor={'rgba(255,255,255,0.7)'}
-            underlineColorAndroid="transparent"
-          />
-        </View>
+    constructor(props) {
+        super(props);
 
-        <View style={styles.inputContainer}>
-          <Icon
-            name={'ios-lock'}
-            size={28}
-            color={'rgba(255,255,255, 0.7)'}
-            style={styles.inputicone}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder={'Email'}
-            placeholderTextColor={'rgba(255,255,255,0.7)'}
-            underlineColorAndroid="transparent"
-          />
-        </View>
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+        };
+    }
 
-        <View style={styles.inputContainer}>
-          <Icon
-            name={'ios-lock'}
-            size={28}
-            color={'rgba(255,255,255, 0.7)'}
-            style={styles.inputicone}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder={'Senha'}
-            secureTextEntry={true}
-            placeholderTextColor={'rgba(255,255,255,0.7)'}
-            underlineColorAndroid="transparent"
-          />
-        </View>
-        <View style={styles.inputContainer}>
-          <Icon
-            name={'ios-lock'}
-            size={28}
-            color={'rgba(255,255,255, 0.7)'}
-            style={styles.inputicone}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder={'Confrimar Senha'}
-            secureTextEntry={true}
-            placeholderTextColor={'rgba(255,255,255,0.7)'}
-            underlineColorAndroid="transparent"
-          />
-        </View>
+    handleName = text => {
+        this.setState({ name: text });
+    };
 
-        
+    handleEmail = text => {
+        this.setState({ email: text });
+    };
 
-        <TouchableOpacity style={styles.btnCadastrar}>
-          <Text style={styles.text}>Cadastar</Text>
-        </TouchableOpacity>
-      </ImageBackground>
-    );
-  }
+    handlePsw = text => {
+        this.setState({ password: text });
+    };
+    render() {
+        return (
+            <ImageBackground source={bgimage} style={styles.backgroundContainer}>
+                <View styles={styles.logoContainer}>
+                    <Image source={logoimage} style={styles.logo} />
+                    <Text style={styles.logoText}>CADASTRO</Text>
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <Icon
+                        name={'ios-person'}
+                        size={28}
+                        color={'rgba(255,255,255, 0.7)'}
+                        style={styles.inputicone}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder={'Username'}
+                        placeholderTextColor={'rgba(255,255,255,0.7)'}
+                        underlineColorAndroid="transparent"
+                    />
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <Icon
+                        name={'ios-lock'}
+                        size={28}
+                        color={'rgba(255,255,255, 0.7)'}
+                        style={styles.inputicone}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder={'Email'}
+                        placeholderTextColor={'rgba(255,255,255,0.7)'}
+                        underlineColorAndroid="transparent"
+                    />
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <Icon
+                        name={'ios-lock'}
+                        size={28}
+                        color={'rgba(255,255,255, 0.7)'}
+                        style={styles.inputicone}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder={'Senha'}
+                        secureTextEntry={true}
+                        placeholderTextColor={'rgba(255,255,255,0.7)'}
+                        underlineColorAndroid="transparent"
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <Icon
+                        name={'ios-lock'}
+                        size={28}
+                        color={'rgba(255,255,255, 0.7)'}
+                        style={styles.inputicone}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder={'Confrimar Senha'}
+                        secureTextEntry={true}
+                        placeholderTextColor={'rgba(255,255,255,0.7)'}
+                        underlineColorAndroid="transparent"
+                    />
+                </View>
+
+
+
+                <TouchableOpacity
+                    style={styles.btnCadastrar}
+                    onPress={() => {
+                        //console.log(this.state);
+
+                        axios({
+                            method: 'post',
+                            url: 'http://localhost/carwash2/public/user',
+                            data: {
+                                name: this.handleName,
+                                email: this.handleEmail,
+                                password: this.handlePsw,
+                            },
+                        });
+                    }}>
+                    <Text style={styles.text}>Cadastar</Text>
+                </TouchableOpacity>
+            </ImageBackground>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  backgroundContainer: {
-    flex: 1,
-    width: null,
-    height: null,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    backgroundContainer: {
+        flex: 1,
+        width: null,
+        height: null,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 
-  logoContainer: {
-    alignItems: 'center',
-    marginBottom: 50,
-  },
+    logoContainer: {
+        alignItems: 'center',
+        marginBottom: 50,
+    },
 
-  logo: {
-    width: 170,
-    height: 120,
-  },
+    logo: {
+        width: 170,
+        height: 120,
+    },
 
-  logoText: {
-    alignItems: 'center',
-    color: 'white',
-    fontSize: 35,
-    fontWeight: '500',
-    marginTop: 0.1,
-    marginBottom: 50,
-    opacity: 0.7,
-  },
+    logoText: {
+        alignItems: 'center',
+        color: 'white',
+        fontSize: 35,
+        fontWeight: '500',
+        marginTop: 0.1,
+        marginBottom: 50,
+        opacity: 0.7,
+    },
 
-  inputContainer: {
-    marginTop: 10,
-  },
+    inputContainer: {
+        marginTop: 10,
+    },
 
-  input: {
-    width: WIDTH - 55,
-    height: 45,
-    borderRadius: 25,
-    fontSize: 16,
-    paddingLeft: 45,
-    backgroundColor: 'rgba(0,0,0,0.35)',
-    color: 'rgba(255,255,255,0.7)',
-    marginHorizontal: 25,
-  },
+    input: {
+        width: WIDTH - 55,
+        height: 45,
+        borderRadius: 25,
+        fontSize: 16,
+        paddingLeft: 45,
+        backgroundColor: 'rgba(0,0,0,0.35)',
+        color: 'rgba(255,255,255,0.7)',
+        marginHorizontal: 25,
+    },
 
-  inputicone: {
-    position: 'absolute',
-    top: 8,
-    left: 37,
-  },
+    inputicone: {
+        position: 'absolute',
+        top: 8,
+        left: 37,
+    },
 
-  btnCadastrar: {
-    width: WIDTH - 55,
-    height: 45,
-    borderRadius: 25,
-    backgroundColor: '#432577',
-    justifyContent: 'center',
-    marginTop: 20,
-  },
+    btnCadastrar: {
+        width: WIDTH - 55,
+        height: 45,
+        borderRadius: 25,
+        backgroundColor: '#432577',
+        justifyContent: 'center',
+        marginTop: 20,
+    },
 
-  text: {
-    color: 'rgba(255,255,255,0.7)',
-    fontSize: 16,
-    textAlign: 'center',
-  },
+    text: {
+        color: 'rgba(255,255,255,0.7)',
+        fontSize: 16,
+        textAlign: 'center',
+    },
 });
